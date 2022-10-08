d3206d3a7ee2037572c794a2cdc3b03a6090654d
Drew Edwards <github@drew.contact>
2022-10-08 15:24:55 +0100

    chore: publish to maven

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
   id("fabric-loom") version "1.0-SNAPSHOT"
   id("io.github.juuxel.loom-quiltflower") version "1.7.3"
   id("maven-publish")
+  id("signing")
 }
 
 val modVersion: String by project
@@ -44,7 +45,8 @@ tasks.withType<KotlinCompile>().configureEach {
 }
 
 repositories {
-  mavenLocal {
+  maven {
+    url = uri("https://repo.lem.sh/releases")
     content {
       includeGroup("pw.switchcraft")
     }
@@ -67,7 +69,7 @@ dependencies {
   }
   modImplementation("net.fabricmc", "fabric-language-kotlin", fabricKotlinVersion)
 
-  modImplementation("pw.switchcraft", "sc-library", scLibraryVersion)
+  modImplementation(include("pw.switchcraft", "sc-library", scLibraryVersion))
 
   // CC: Restitched
   modApi("com.github.cc-tweaked:cc-restitched:${ccVersion}") {
@@ -141,3 +143,26 @@ tasks {
     }
   }
 }
+
+publishing {
+  publications {
+    register("mavenJava", MavenPublication::class) {
+      from(components["java"])
+    }
+  }
+
+  repositories {
+    maven {
+      name = "lemmmyRepo"
+      url = uri("https://repo.lem.sh/releases")
+      credentials {
+        username = System.getenv("MAVEN_USERNAME")
+        password = System.getenv("MAVEN_PASSWORD")
+      }
+      authentication {
+        create<BasicAuthentication>("basic")
+      }
+    }
+  }
+}
+
