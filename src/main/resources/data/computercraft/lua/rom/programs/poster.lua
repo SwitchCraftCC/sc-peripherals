b8239ddc08910b926ff4ff9e9629b5b3fbcc70a0
Sammy <xX1SuperCoolerBoiXx@Gmail.com>
2023-04-08 23:11:48 +0200

    feat: .2dja support for poster.lua
    feat: .2dja support for poster.lua
    
    Add support for .2dja files, a file format used by the community to simplify the printing of large signs, which essentially contains multiple .2dj files.
    
    Additionally, the status text has been updated.
    
    While maintaining compatibility, two optional numbers can now be appended to the syntax to indicate the interval of pages to be used if a .2dja file is provided. Or a single specific page.
    
    This commit also ensures that the program is now compatible with normal PCs that don't support all colors.
    
    Finally, comments and style changes have been included for improved readability.
    
    fix: Removed filename completion and added Index out of bounds check
    
    Removed code that searched for similar-looking files and replaced it with an error message.
    
    Also added an error check for when the given bounds exceed the maximum number of pages in the table.
    
    Additionally, made some minor code style changes including:
    * Removing Hungarian notation
    * Using %d for numbers instead of %s
    * Standardizing comma spacing to one space
    * More consistent error patterns
    * Ensuring proper grammar.
    
    fix: Status text wont exit after first print and chore: Text alignement
    
    Because I renamed all variables to be more consistent, there were some redeclarations that accured the program in exiting after the first print.
    
    In addition, formating got corrected to  use %s, %d or %f more properly, including spacing.
    
    chore: Text Alignment and number formating
    
    Text Alignment for the statuses is more space efficient.
    
    Instead of using hardcoded sizes for height, the actual height is used now.
    
    Because the percentages are perfectly rounded anyways (even without math.floor), %d is used instead.
    
    fix: poster.lua syntax for intervals and filename completion
    
    The following bugs, mostly realted with the syntax got fixed:
    
    * If a valid third argument is given without a fourth, it is taken as an index for .2dja files. If a fourth argument is also given, it is interpreted as an interval for it. If no arguments are provided, all pages are assumed to be printed. If an invalid argument is provided, the usage is printed.
    * If a filename is provided without an extension, a file with a known extension will be searched for. If multiple files are found, or none are found, an corresponding error message is displayed. If a single file is found, it is used as intended.
    
    Finally, some printed messages were changed too, such as the title and final message indicating the number of posters printed."

--- src/main/resources/data/computercraft/lua/rom/programs/poster.lua
+++ src/main/resources/data/computercraft/lua/rom/programs/poster.lua
@@ -1,10 +1,29 @@
-local startColor = term.getTextColor()
+local startTColor = term.getTextColor()
+
+local highlightColor = colors.lightGray
+local errColor = colors.gray
+local greenColor = colors.white
+
+if term.isColor() then
+  highlightColor = colors.yellow
+  errColor = colors.red
+  greenColor = colors.green
+end
 
 local function printUsage()
   local programName = arg[0] or fs.getName(shell.getRunningProgram())
-  print("Usage: ")
-  print(programName .. " <file.2dj or url> [count]")
-  print(programName .. " stop")
+
+  term.setTextColor(colors.lightGray)
+  term.write("Usage: ")
+
+  term.setTextColor(colors.white)
+  print(("%s <file or url> [count] [options?]"):format(programName))
+
+  term.setTextColor(colors.gray)
+  print("Options for .2dja: [ <index> | <from> <to> ]")
+
+  term.setTextColor(colors.white)
+  print(("Type '%s stop' to cancel the print task"):format(programName))
 end
 
 -- Find a printer
@@ -15,7 +34,8 @@ end
 
 -- Validate arguments
 local args = { ... }
-if #args < 1 or #args > 2 then
+if #args < 1 or #args > 4
+or args[1]:lower() == "help" then
   printUsage()
   return
 end
@@ -27,68 +47,146 @@ if args[1] == "stop" then
   return
 end
 
+local status = printer.status()
+if status == "busy" then
+  error("Printer is busy", 0)
+end
+
+-- Convert string to number and validate it
+local function getNum(str)
+  local num = tonumber(str)
+  if not num or math.floor(num) < 1 then
+    return
+  end
+  return math.floor(num)
+end
+
+-- Get count
 local count = 1
-if #args == 2 then
-  count = tonumber(args[2])
-  if not count or math.floor(count) < 1 then
+if #args > 1 then
+  count = getNum(args[2])
+  if not count then
     printUsage()
     return
   end
-  count = math.floor(count)
 end
 
--- Load the 2dj file
-local filename = args[1]
+-- Get index, if given
+local index = { all = true, from = nil, to = nil }
+if #args >= 3 then
+  local num1, num2 = getNum(args[3]), getNum(args[4])
+  if num1 and num2 then
+    -- Interval
+    index.from = math.min(num1, num2)
+    index.to = math.max(num1, num2)
+  elseif #args > 3 then
+    -- Second argument is not acceptable
+    printUsage()
+    return
+  elseif num1 then
+    -- Index, not interval
+    index.from = num1
+    index.to = num1
+  else
+    -- First argument is not acceptable
+    printUsage()
+    return
+  end
 
-local function load2djFile(name)
-  local f
+  -- Index or interval is given, therefore we do not want to print all pages
+  index.all = false
+end
+
+local filename = args[1]
+-- Loads from local file or Url
+local function loadPoster(name)
+  local stream
+
   if http and name:match("^https?://") then
     print("Downloading...")
-    f = http.get{ url = name, binary = true }
+    stream = http.get{ url = name, binary = true }
   else
+    print("Loading...")
     name = shell.resolve(name)
     if not fs.exists(name) then
-      -- Try to find a .2dj file if the user didn't specify the extension
-      if not filename:match(".2dj$") then
-        local newName = name .. ".2dj"
-        if fs.exists(newName) then
-          return load2djFile(newName)
+      -- Try to find a .2dj or .2dja file if the extension did not got specified
+      if not filename:match(".2dj$") or not filename:match(".2dja$") then
+        local hits = 0
+        local newName
+
+        for _, sEnd in ipairs({".2dj", ".2dja"}) do
+          local otherName = name..sEnd
+          if fs.exists(otherName) then
+            newName = otherName
+            hits = hits+1
+          end
+        end
+        if hits == 1 then
+          return loadPoster(newName)
+        elseif hits > 1 then
+          error(("File extension for \'%s\' is not specified."):format(name), 0)
         end
       end
-      error("File not found: " .. name, 0)
+      error(("File \'%s\' not found."):format(name), 0)
     end
-    f = fs.open(name, "r")
+    stream = fs.open(name, "r")
   end
-  
-  if not f then
-    error("Could not open " .. name, 0)
+
+  if not stream then
+    error(("Could not open \'%s\'"):format(name), 0)
   end
-  local data = f.readAll()
-  f.close()
+
+  local data = stream.readAll()
+  stream.close()
 
   local data, err = textutils.unserialiseJSON(data)
-  if data == nil then
-    error("\n\nCould not parse " .. name .. ": " .. err, 0)
+  if type(data) ~= "table" then
+    error(("\n\nCould not parse \'%s\': %s"):format(name, err), 0)
   end
   return data
 end
 
-local printData = load2djFile(filename)
-if not printData.pixels then
-  error("Invalid print data - missing pixels")
+-- Check if given poster is valid
+local function isValidPoster(data)
+  if type(data.pixels) ~= "table" then
+    return true, "Table \'pixels\' does not exist"
+  elseif type(data.palette) ~= "table" then
+    return true, "Table \'palette\' does not exist"
+  end
+
+  return false
 end
 
--- Output some basic print metadata (models often have credit in tooltips)
-term.write("Printing: ")
-term.setTextColor(colors.lightGray)
-print(printData.label or filename)
+local posters = {}
+local data = loadPoster(filename)
+-- Add all needed pages to stack (.2dja)
+if type(data.pages) == "table" then
+  if index.all then
+    index.from = 1
+    index.to = #data.pages
+  elseif index.to > #data.pages then
+    error(("Index out of bounds (got %d, limit: %d)"):format(index.to, #data.pages), 0)
+  end
 
-if printData.tooltip then
-  term.write(printData.tooltip)
+  for i=index.from, index.to do
+    local poster = data.pages[i]
+    local hasError, msg = isValidPoster(poster)
+    if hasError then
+      error(("Could not load page %d: \'%s\'"):format(i, msg), 0)
+    end
+
+    table.insert(posters, poster)
+  end
+else
+  -- Must be a single poster (.2dj)
+  local hasError, msg = isValidPoster(data)
+  if hasError then
+    error(("Could not load .2dj file: \'%s\'"):format(msg), 0)
+  end
+  table.insert(posters, data)
 end
 
-print("")
-
+-- Print a given poster
 local function commitPrint(data)
   printer.reset()
 
@@ -101,82 +199,89 @@ local function commitPrint(data)
   printer.commit(count)
 end
 
-local function percent(n, m)
-  return math.ceil(n / m * 100)
-end
-
-local function writePercent(label, n, m)
-  local level = percent(n, m)
-
-  term.setTextColor(colors.yellow)
-  term.write(label)
-  
-  term.setTextColor(level >= 10 and colors.white or colors.red)
-  term.write(level .. "%  ")
-end
-
-local remaining = count
-local function showPrintStatus()
-  local _, y = term.getCursorPos()
-  local ink, maxInk = printer.getInkLevel()
-
-  term.setCursorPos(1, y - 1)
-  term.clearLine()
-  writePercent("Ink: ", ink, maxInk)
-  
-  term.setCursorPos(1, y)
-  term.clearLine()
-  term.setTextColor(colors.yellow)
-  term.write("Printing... ")
-  term.setTextColor(colors.white)
-  term.write(math.min((count - remaining) + 1, count) .. " / " .. count)
-
-  term.setCursorPos(1, y)
-end
-
--- Begin printing
-local status = printer.status()
-if status == "busy" then
-  error("Printer is busy", 0)
-end
-
-commitPrint(printData)
-
-term.setTextColor(colors.yellow)
+-- Start printing (Finally...)
+term.setTextColor(highlightColor)
 print("\nHold Ctrl+T to stop\n")
-showPrintStatus()
 
-while true do
-  local e, p1 = os.pullEventRaw()
-  local n = math.min(math.max((count - remaining) + 1, 1), count)
+term.setTextColor(colors.lightGray)
+print("Now printing:")
 
-  if e == "poster_printer_complete" or e == "poster_printer_state" then
-    remaining = p1
-    showPrintStatus()
+local _, ypos = term.getCursorPos()
+local width, height = term.getSize()
 
-    if remaining <= 0 then
-      -- Printing complete, print the count we managed to print
-      local _, y = term.getCursorPos()
-      term.setCursorPos(1, y)
-      term.clearLine()
-      term.setTextColor(colors.green)
-      print("Printed " .. n .. " item" .. (n ~= 1 and "s" or ""))
+-- Move everything up for enough space
+if ypos > height-3 then
+  term.scroll(4)
+  ypos = 15
+end
 
-      break
-    end
-  elseif e == "terminate" then    
-    -- Halt printing and print the count we managed to print
-    printer.stop()
-
-    local _, y = term.getCursorPos()
-    term.setCursorPos(1, y)
-    term.clearLine()
-    term.setTextColor(colors.red)
-    print("Printing terminated. Printed " .. n .. " item" .. 
-      (n ~= 1 and "s" or "") .. " out of " .. count)
-
-    break 
+local function printRow(name, value, optional)
+  term.setTextColor(highlightColor)
+  term.write(name)
+  term.setTextColor(colors.white)
+  term.write(value)
+  if optional then
+    term.setTextColor(colors.lightGray)
+    term.write(optional)
   end
 end
 
-term.setTextColor(startColor)
+-- Loop to print all selected posters
+local printed = 0
+local maxPages = #posters*count
+local numDigits = math.floor(math.log10(maxPages))+1
+local terminated = false
+
+for i, data in ipairs(posters) do
+  local currentPage = 0
+  if terminated then break end
+  commitPrint(data)
+
+  term.setCursorPos(1, ypos)
+  printRow("Name: ", (data.label or "Untitled"):sub(1, width-6))
+
+  term.setCursorPos(1, ypos+1)
+  printRow("Status: ", ("  0%% : %"..numDigits.."d / %"..numDigits.."d"):format(printed, maxPages), (" (x%d)       "):format(count))
+
+
+  local cur, max = printer.getInkLevel()
+  term.setCursorPos(1, ypos+2)
+  printRow("Ink: ", ("%3d%%   "):format(math.floor(100/max*cur)))
+
+  -- Status and Ink level
+  while true do
+    local e, p1 = os.pullEventRaw()
+
+    if e == "poster_printer_complete" or e == "poster_printer_state" then
+      local progress = 100-(tonumber(p1) or 0)
+
+      if progress >= 100 then
+        printed = printed+1
+        currentPage = currentPage+1
+      end
+
+      term.setCursorPos(1, ypos+1)
+      printRow("Status: ", ("%3d%% : %"..numDigits.."d / %"..numDigits.."d "):format(progress, printed, maxPages), ("(x%d)       "):format(count))
+      if currentPage >= count then
+        break
+      end
+    elseif e == "terminate" then
+      printer.stop()
+      terminated = true
+      break
+    end
+  end
+end
+
+-- Print results
+term.setCursorPos(1, ypos+3)
+local plural = (printed ~= 1) and "posters" or "poster"
+if terminated then
+  plural = (printed+1 ~= 1) and "posters" or "poster"
+  term.setTextColor(errColor)
+  print(("Printed %d %s out of %d."):format(printed+1, plural, maxPages))
+else
+  term.setTextColor(greenColor)
+  print(("Printed %d %s!"):format(printed, plural))
+end
+term.setTextColor(startTColor)
